5a6b4d4436f96b5bab4ae99870d292a964717b6a
Morgan Bentell <morgan.bentell@evry.com>
2019-06-17 21:28:44 +0200

    Add ysera dep. bump clj and cljs.

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/agentbellnorm/dativity"
   :license {:name "MIT"
             :url "https://github.com/agentbellnorm/dativity/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ysera "1.3.0"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [ubergraph "0.5.1"]]
   :test-paths ["test"]
   :source-paths ["src"]
