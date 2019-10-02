f754b932f7c39b321dd157af1fea32b60447a6a5
Morgan Bentell <morgan.bentell@evry.com>
2019-10-02 06:16:36 +0200

    bump ysera and change project version.

--- project.clj
+++ project.clj
@@ -1,16 +1,11 @@
-(defproject dativity "2.1.0"
+(defproject dativity "2.2.0-SNAPSHOT"
   :description "stateless, data driven process engine"
   :url "https://github.com/agentbellnorm/dativity"
   :license {:name "MIT"
             :url "https://github.com/agentbellnorm/dativity/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ysera "2.0.1"]
+                 [ysera "2.0.2"]
                  [org.clojure/clojurescript "1.10.520"]
                  [ubergraph "0.8.1"]]
   :test-paths ["test"]
-  :source-paths ["src"]
-  :plugins [[lein-cljsbuild "1.1.7"]]
-  :cljsbuild {
-              :builds [{:source-paths ["src"]
-                        :compiler {:optimizations :whitespace
-                                   :pretty-print true}}]})
+  :source-paths ["src"])
